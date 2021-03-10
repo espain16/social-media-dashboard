44c851806e5cf9de04227005c88b373f47138da8
Elise Spain <elisespain@gmail.com>
2021-03-09 22:25:45 -0800

    Dark mode functionality complete and Sass root variables applied

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 //add and event listener to the btn 
   // once clicked the event listener will display something in the console 
 
-let toggleBtn, dashboard;
-dashboard = document.getElementById('dashboard');
-toggleBtn = document.querySelector(".toggler-btn");
-toggleBtn.addEventListener("click", function() {
-  dashboard.classList.toggle('.dark-mode');
-  console.log(`I've been clicked successfully`);
+
+
+document.querySelector('input[data-switch-dark]').addEventListener("click", function() {
+  document.body.classList.toggle('dark');
+  console.log("clicked");
 
 })
 
