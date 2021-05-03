44afbac377d3c5cb6d3b5c6924d265213033d618
Elise Spain <elisespain@gmail.com>
2021-05-02 18:41:04 -0700

    Removed unused files from project

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-//select the body of the site 
-//select the toggle btn 
-//add and event listener to the btn 
-  // once clicked the event listener will display something in the console 
-
-
-
+//adding an event listener for the toggle 
 document.querySelector('input[data-switch-dark]').addEventListener("click", function() {
   document.body.classList.toggle('dark');
   
